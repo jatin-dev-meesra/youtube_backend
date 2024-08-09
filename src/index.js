1ada4e9bf279504b6ab4d70d22682a79f95f1dcb
jatin-dev-meesra <jatin@meesra.com>
2024-08-09 13:39:03 +0530

    utils files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port :${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO DB Connection failed !!!", error);
+  });
 
 /*
 import express from "express";
